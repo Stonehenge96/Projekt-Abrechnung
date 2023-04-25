da61e46611534a1069ceb452f44e56db84e84baf
Stonehenge96 <soeren.zanders@gmail.com>
2023-04-25 10:40:50 +0100

    erster versuch mit dem einlesen von text file

--- Vector to String/Quelle.cpp
+++ Vector to String/Quelle.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <sstream>
 #include <fstream>
+#include <filesystem>
 
 using namespace std;
 
@@ -34,7 +35,7 @@ ostream& operator << (ostream& output, Verbrauch& v)
 
 /*istream& operator >> (istream& input, Verbrauch& v)
 {
-	input << v.day << ' ' << v.month << ' ' << v.year << ' ' << v.zaehlerstand << ' ' << v.preis;
+	input >> v.day >> ' ' >> v.month >> ' ' >> v.year >> ' ' >> v.zaehlerstand >> ' ' >> v.preis;
 	return input;
 }*/
 
@@ -44,11 +45,16 @@ string Umwandlung( vector <Verbrauch>& Abrechnung)
 	
 	for (unsigned i = 0; i < Abrechnung.size(); i++)
 	{
-		ss << Abrechnung[i];
+		ss << Abrechnung[i] << '\n';
 	}
 
 	return ss.str();
 }
+
+/*stringstream Umwandlung2(string str)
+{
+	for 
+}*/
 
 
 int main()
@@ -59,8 +65,12 @@ int main()
 	if (Liste.is_open())
 	{
 		Verbrauch added = { 14, 3, 2023, 100, 50 };
+		Verbrauch add1 = { 20, 4, 2023, 110, 60 };
+		Verbrauch add2 = { 25, 4, 2023, 120, 70 };
 
 		Abrechnung.push_back(added);
+		Abrechnung.push_back(add1);
+		Abrechnung.push_back(add2);
 		
 		string str = Umwandlung(Abrechnung);
 
@@ -73,21 +83,35 @@ int main()
 	}	
 	Liste.close();
 
-	for (unsigned i = 0; i < Abrechnung.size(); i++)
-	{
-		//cout << Abrechnung[i] << endl;
-	}
-
 	Abrechnung.clear();
 
 	Liste.open("Test.txt", ios::app | ios::out | ios::in);
 
 	if (Liste.is_open())
 	{
-		istringstream ss;
-		ss >> Liste.rdbuf();
-		string str = ss.str();
+		//Verbrauch Neu = { .day = 0, .month = 0, .year = 0, .zaehlerstand = 0.0, .preis = 0.0 };
+		Verbrauch Neu = { 0, 0, 0, 0.0, 0.0 };
+		string str;
+		ifstream istream("Test.txt");
+		stringstream ss;
 
-		cout << str << endl;
+		while (getline(istream, str))
+		{
+			cout << str << endl;
+		}
+	
+
+		/*while (ss >> Neu)
+		{
+			Abrechnung.push_back(Neu);
+		}
+		
+		for (unsigned i = 0; i < Abrechnung.size(); i++)
+		{
+			cout << Abrechnung[i] << endl;
+		}*/
+		
+		
+		
 	}
 } 
